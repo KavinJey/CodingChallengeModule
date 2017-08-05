8ca0e8f9dc08103558830b91af5354dbb9f96851
Kavin Jey <jeyakavin@hotmail.com>
2017-08-05 17:18:41 -0400

    Added Dijkstra's

--- functions.py
+++ functions.py
@@ -220,7 +220,8 @@ def grid(x, y):
 
     return arr
 
-def palindrome(num):
+
+def isPalindrome(num):
     if num[::-1] == num:
         return True
 
@@ -236,7 +237,7 @@ def subset_sum(numbers, target, partial=[]):
 
     # check if the partial sum is equals to target
     if s == target:
-        print "sum(%s)=%s" % (partial, target)
+        print("sum(%s)=%s" % (partial, target))
     if s >= target:
         return  # if we reach the number why bother to continue
 
@@ -244,6 +245,87 @@ def subset_sum(numbers, target, partial=[]):
         n = numbers[i]
         remaining = numbers[i+1:]
         subset_sum(remaining, target, partial + [n])
+
+
+
+#Dijkstra's Algorithm
+#Taken from: https://gist.github.com/econchick/4666413
+class Graph:
+  def __init__(self):
+    self.nodes = set()
+    self.edges = defaultdict(list)
+    self.distances = {}
+
+  def add_node(self, value):
+    self.nodes.add(value)
+
+  def add_edge(self, from_node, to_node, distance):
+    self.edges[from_node].append(to_node)
+    self.edges[to_node].append(from_node)
+    self.distances[(from_node, to_node)] = distance
+
+
+def dijsktra(graph, initial):
+  visited = {initial: 0}
+  path = {}
+
+  nodes = set(graph.nodes)
+
+  while nodes:
+    min_node = None
+    for node in nodes:
+      if node in visited:
+        if min_node is None:
+          min_node = node
+        elif visited[node] < visited[min_node]:
+          min_node = node
+
+    if min_node is None:
+      break
+
+    nodes.remove(min_node)
+    current_weight = visited[min_node]
+
+    for edge in graph.edges[min_node]:
+      weight = current_weight + graph.distance[(min_node, edge)]
+      if edge not in visited or weight < visited[edge]:
+        visited[edge] = weight
+        path[edge] = min_node
+
+  return visited, path
+
+
+#Or This implementation of Dijkstra's
+nodes = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
+distances = {
+    'B': {'A': 5, 'D': 1, 'G': 2},
+    'A': {'B': 5, 'D': 3, 'E': 12, 'F' :5},
+    'D': {'B': 1, 'G': 1, 'E': 1, 'A': 3},
+    'G': {'B': 2, 'D': 1, 'C': 2},
+    'C': {'G': 2, 'E': 1, 'F': 16},
+    'E': {'A': 12, 'D': 1, 'C': 1, 'F': 2},
+    'F': {'A': 5, 'E': 2, 'C': 16}}
+
+unvisited = {node: None for node in nodes} #using None as +inf
+visited = {}
+current = 'B'
+currentDistance = 0
+unvisited[current] = currentDistance
+
+while True:
+    for neighbour, distance in distances[current].items():
+        if neighbour not in unvisited: continue
+        newDistance = currentDistance + distance
+        if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
+            unvisited[neighbour] = newDistance
+    visited[current] = currentDistance
+    del unvisited[current]
+    if not unvisited: break
+    candidates = [node for node in unvisited.items() if node[1]]
+    current, currentDistance = sorted(candidates, key = lambda x: x[1])[0]
+
+print(visited)
+
 
 
 #Still needed:
